cc5d5a8125555bc2ec333b24e8944796bf041fc3
BlaSee01 <92850744+BlaSee01@users.noreply.github.com>
2021-11-17 16:12:53 +0100

    1JM

--- 1JM/main.c
+++ 1JM/main.c
@@ -3,6 +3,25 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int n = 80567;
+
+    do{
+        n++;
+        if(n%8==0){
+            printf("0 ");
+            if(n%2!=0){
+                printf(", jest nieparzysta \n");
+            }else{
+                printf("\n");
+            }
+        }else{
+            if(n%2!=0){
+                printf("%d , jest nieparzysta \n" , n);
+            }else{
+                printf("%d\n" , n);
+            }
+        }
+        }while(n<80579);
+
     return 0;
 }
